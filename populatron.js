03e3c4d96046ec0bde2269977a6f3762353e824c
Stefano Demichelis <steppashr@gmail.com>
2019-07-18 02:15:22 +0900

    populatron, finished. added csv-parser

--- populatron.js
+++ populatron.js
@@ -1,5 +1,31 @@
 const fs = require("fs");
+const csv = require("csv-parser");
+
+const solving = () => {
+  return new Promise((resolve) => {
+    fs
+      .createReadStream("cities.csv")
+      .pipe(csv())
+      .on("data", function(data) {
+        try {
+          populatron(data);
+        } catch (err) {
+          return err;
+        }
+      })
+      .on("end", () => {
+        resolve(result);
+      });
+  });
+};
+
+let result = 0;
+async function populatron(data) {
+  result += parseInt(data.population);
+}
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    solving().then((final) => onFinished(final));
+  },
 };
